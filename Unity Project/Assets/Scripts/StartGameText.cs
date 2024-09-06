bdc72c2ae2803a6e210007965fcc486e887e7743
ROC-Kenneth <87307361+Xearoph@users.noreply.github.com>
2024-09-06 10:01:35 +0200

    Update StartGameText.cs

--- Unity Project/Assets/Scripts/StartGameText.cs
+++ Unity Project/Assets/Scripts/StartGameText.cs
@@ -2,6 +2,7 @@
 
 public class StartGameText : MonoBehaviour
 {
+    // Comments zijn niet leuk om te schrijven, maar wel heel handig als ze nuttig zijn.. quak.
     BallMovement ballMovement;
     [SerializeField] GameObject playText;
     void Start()
